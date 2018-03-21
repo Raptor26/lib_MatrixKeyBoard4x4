f8ac0e154342309496cebd34a7b5962a292f8825
Isaev Mickle <mrraptor26@gmail.com>
2018-03-21 12:31:21 +0300

    docs (Lib_A_VTMR_virtual_timers) дополнительные примеры использования таймеров;

--- Lib_A_VTMR_virtual_timers.c
+++ Lib_A_VTMR_virtual_timers.c
@@ -9,46 +9,55 @@
  *          - функция, разрешающая отсчет времени;
  *          - функция, останавливающая отсчет времени;
  *          - функция, сбрасывающая значение счетчика и разрешающая отсчет времени;
- *          - функция, инкрементирующая программный счетчик;
+ *          - функция, инкрементирующая виртуальный таймер;
  *
  *      @note   При таком способе отсчета времени инкремент виртуального таймера
  *              выполняется при вызове функции VTMR_IntProcess();
  *      @code
  *          // Объявление глобальных переменных
- *          VTMR_tmr_s myVirtTimerStruct;
- *          uint32_t myVirtTimerValue;
+ *          VTMR_tmr_s myVirtTimer1CntStruct,
+ *                     myVirtTimer2CntStruct;
+ *          uint32_t myVirtTimer1Value,
+ *                   myVirtTimer2Value;
  *
  *          int main(void)
  *          {
  *              ...
  *              // Разрешает инкремент виртуального таймера при вызове
  *              // функции VTMR_IntProcess();
- *              VTMR_StartVirtTimer(&myVirtTimerStruct);
+ *              VTMR_StartVirtTimer(&myVirtTimer1CntStruct);
+ *              VTMR_StartVirtTimer(&myVirtTimer2CntStruct);
  *              ...
  *              while(1)
  *              {
  *                  // Разрешает инкремент виртуального таймера при вызове
  *                  // функции VTMR_IntProcess() и сбрасывает значение
  *                  // счетчика в нуль - перезапуск виртуального таймера;
- *                  VTMR_RestartVirtTimer(&myVirtTimerStruct);
+ *                  VTMR_RestartVirtTimer(&myVirtTimer1CntStruct);
+ *                  VTMR_RestartVirtTimer(&myVirtTimer2CntStruct);
  *                  ...
  *                  // Получение количества вызова функции VTMR_IntProcess();
  *                  // (получение количества прерываний  "void TIMER_1_INTERRUPT(void)")
- *                  myVirtTimerValue = VTMR_GetValueVirtTimer(&myVirtTimerStruct);
+ *                  myVirtTimer1Value = VTMR_GetValueVirtTimer(&myVirtTimer1CntStruct);
+ *                  ...
+ *                  myVirtTimer2Value = VTMR_GetValueVirtTimer(&myVirtTimer2CntStruct);
  *
  *                  // Причем
- *                  myVirtTimerValue == myVirtTimerStruct.timeInterval;
+ *                  myVirtTimer1Value == myVirtTimer1CntStruct.timeInterval;
+ *                  myVirtTimer2Value == myVirtTimer2CntStruct.timeInterval;
  *
  *                  // Запрещает инкремент виртуального таймера при вызове
  *                  // функции VTMR_IntProcess() - остановка виртуального таймера;
- *                  VTMR_StopVirtTimer(&myVirtTimerStruct);
+ *                  VTMR_StopVirtTimer(&myVirtTimer1CntStruct);
+ *                  VTMR_StopVirtTimer(&myVirtTimer2CntStruct);
  *              }
  *          }
  *
  *          void TIMER_1_INTERRUPT(void)
  *          {
  *              ...
- *              VTMR_IntProcess(&myVirtTimerStruct);
+ *              VTMR_IntProcess(&myVirtTimer1CntStruct);
+ *              VTMR_IntProcess(&myVirtTimer2CntStruct);
  *          }
  *      @endcode
  *          --------------------------------------------------------------------
@@ -64,23 +73,31 @@
  *
  *      @code
  *          // Объявление глобальных переменных
- *          VTMR_tmr_s myVirtTimerStruct;
- *          uint32_t myVirtTimerValue;
+ *          VTMR_tmr_s myVirtTimer1CntStruct,
+ *                     myVirtTimer2CntStruct;
+ *          uint32_t myVirtTimer1Value,
+ *                   myVirtTimer2Value;
  *
  *          int main(void)
  *          {
  *              ...
- *              // Пример для микроконтроллеров PIC:
+ *              // Пример инициализации для микроконтроллеров PIC:
  *              // где TMR9 - указатель на старшие 16 бит аппаратного счетчика;
  *                     TMR8 - указатель на младшие 16 бит аппаратного счетчика;
- *              VTMR_InitTimerStruct(&myVirtTimerStruct,
+ *              VTMR_InitTimerStruct(&myVirtTimer1CntStruct,
+ *                                   (uint32_t*) &TMR9,
+ *                                   (uint32_t*) &TMR8);
+ *              VTMR_InitTimerStruct(&myVirtTimer2CntStruct,
  *                                   (uint32_t*) &TMR9,
  *                                   (uint32_t*) &TMR8);
  *
- *              // Пример для микроконтроллеров STM32;
+ *              // Пример инициализации для микроконтроллеров STM32;
  *              // где TIM3->CNT - указатель на старшие 16 бит аппаратного счетчика;
  *                     TIM4->CNT - указатель на младшие 16 бит аппаратного счетчика;
- *              VTMR_InitTimerStruct(&myVirtTimerStruct,
+ *              VTMR_InitTimerStruct(&myVirtTimer1CntStruct,
+ *                                   (uint32_t*) &TIM3->CNT,
+ *                                   (uint32_t*) &TIM4->CNT);
+ *              VTMR_InitTimerStruct(&myVirtTimer2CntStruct,
  *                                   (uint32_t*) &TIM3->CNT,
  *                                   (uint32_t*) &TIM4->CNT);
  *              ...
@@ -89,25 +106,29 @@
  *                  // Запись в переменную счетчика текущего значения
  *                  // аппаратного 32-х битного счетчика;
  *                  // @see VTMR_tmr_s;
- *                  VTMR_StartTimer(&myVirtTimerStruct);
+ *                  VTMR_StartTimer(&myVirtTimer1CntStruct);
+ *                  VTMR_StartTimer(&myVirtTimer2CntStruct);
  *                  ...
  *
  *                  // Определение разницы между текущим значением аппаратного
  *                  // 32-х битного счетчика и значением переменной счетчика в
- *                  // структуре myVirtTimerStruct.
+ *                  // структуре myVirtTimer1CntStruct.
  *                  // В результате имеем количество тиков аппаратного счетчика
  *                  // между вызовами функций VTMR_StartTimer() и VTMR_GetTimerValue();
  *                  // @see VTMR_tmr_s;
- *                  myVirtTimerValue = VTMR_GetTimerValue(&myVirtTimerStruct);
+ *                  myVirtTimer1Value = VTMR_GetTimerValue(&myVirtTimer1CntStruct);
+ *                  ....
+ *                  myVirtTimer2Value = VTMR_GetTimerValue(&myVirtTimer2CntStruct);           
  *
  *                  // Причем
- *                  myVirtTimerValue == myVirtTimerStruct.timeInterval;
+ *                  myVirtTimer1Value == myVirtTimer1CntStruct.timeInterval;
+ *                  myVirtTimer2Value == myVirtTimer2CntStruct.timeInterval;
  *                  ...
  *
- *                  // Если myVirtTimerStruct.cnt = 2^32 - 4,
+ *                  // Если myVirtTimer1CntStruct.cnt == 2^32 - 4,
  *                  // и *pHighCntReg == 0,
- *                  // и *pLowCntReg == 5, то функция VTMR_GetTimerValue()
- *                  // вернет значение 9;
+ *                  // и *pLowCntReg == 5,
+ *                  // то функция VTMR_GetTimerValue() вернет значение 9;
  *              }
  *          }
  *      @endcode
